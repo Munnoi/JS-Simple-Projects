097ce87e2d19772ba45ad823b3fcbe0845176e97
Munnoi <89596902+Munnoi@users.noreply.github.com>
2025-01-17 20:42:31 +0530

    Add files via upload

---
+++ Day87-Palindrome_checker/script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function check() {
+  const value = input.value;
+  const reverse = reverseString(value);
+
+  if (value === reverse) alert("Palindrome");
+  else alert("Not palindrome");
+
+  input.value = ""; // clearing the input.
+}
